32f2af9960d69a1dfedd2468f23940aab9d4a3c3
Olivia-Debora Moisi <deboramoisi@yahoo.com>
2020-10-07 10:11:46 +0300

    Am adaugat MyClass

---
+++ MyFirstApp/MyClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstApp
+{
+    class MyClass
+    {
+        static void main(String[] args)
+        {
+            Console.WriteLine("This is my new class that will be added to the second branch");
+        }
+    }
+
+}
